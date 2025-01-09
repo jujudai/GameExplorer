139eccaffb41cc3f0e7c630a2f1802f24d4a2513
jujudai <takasawad0506gmail.com>
2025-01-09 07:07:05 +0000

    ゲストログイン機能のみ追加、NEXT[モデルの作成.ユーザーページの作成]

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,10 @@ Rails.application.routes.draw do
   sign_out_via: [:delete]
   root to: "homes#top"
   get '/homes/about', to: 'homes#about', as: 'about'
+
+  devise_scope :user do
+    post "users/guest_sign_in", to: "users/sessions#guest_sign_in"
+  end
+
+  resources :users, only: [:show]
 end

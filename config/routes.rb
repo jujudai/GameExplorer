1202e4d8fb0f3564b2a7620e5324e4a30fd3c31d
jujudai <takasawad0506gmail.com>
2025-01-26 08:36:19 +0000

    管理者機能の製作・更新

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
   devise_for :admin, skip: [:registrations, :password], controllers: {
     sessions: 'admin/sessions'
   }
- 
+  
   namespace :admin do
     get 'dashboards', to: 'dashboards#index'
     resources :users, only: [:index, :edit, :update, :destroy]
